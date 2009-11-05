18b86b99b7729835e08901ff13a6d5f277d33b50
paladiy <OlexanderPaladiy@gmail.com>
2009-11-05 15:57:55 +0200

    namespace to options added

--- lib/action_controller/session/redis_session_store.rb
+++ lib/action_controller/session/redis_session_store.rb
@@ -6,15 +6,18 @@ begin
       class RedisSessionStore < AbstractStore
         def initialize(app, options = {})
           super
-          @options = {:host => '127.0.0.1',:port => '6379'}.merge(options)
+          @options = {
+            :host => '127.0.0.1',
+            :port => '6379',
+            :namespace => 'redis_session_store'
+          }.merge(options)
           @server = Redis.new @options
           super
         end
         private
         def get_session(env, sid)
-          puts sid
           begin
-            session = @server.get(sid)
+            session = @server.get(sid_with_namespace(sid))
           rescue Errno::ECONNREFUSED
             session = {}
           end
@@ -22,7 +25,7 @@ begin
         end
 
         def set_session(env, sid, session_data)
-          @server.set(sid, marshal(session_data) )
+          @server.set(sid_with_namespace(sid), marshal(session_data) )
           return true
         rescue Errno::ECONNREFUSED
           return false
@@ -38,6 +41,9 @@ begin
             {}
           end
         end        
+        def sid_with_namespace(sid)
+          @options[:namespace] + sid
+        end
       end
     end
   end
