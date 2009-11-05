a8c07a9e074c61d7aca7e2e955eee70b14b44d48
paladiy <OlexanderPaladiy@gmail.com>
2009-11-05 15:49:41 +0200

    serialization changed from YAML to Marshal

--- lib/action_controller/session/redis_session_store.rb
+++ lib/action_controller/session/redis_session_store.rb
@@ -1,13 +1,10 @@
 # Redis-session-store
 begin
   require_library_or_gem 'redis'
-
   module ActionController
     module Session
       class RedisSessionStore < AbstractStore
         def initialize(app, options = {})
-          # Support old :expires option
-          options[:expire_after] ||= options[:expires]
           super
           @options = {:host => '127.0.0.1',:port => '6379'}.merge(options)
           @server = Redis.new @options
@@ -31,20 +28,19 @@ begin
           return false
         end
         def marshal(data)
-          ActiveSupport::Base64.encode64(YAML.dump(data)) if data
+          ActiveSupport::Base64.encode64(Marshal.dump(data)) if data
         end
 
         def unmarshal(data)
           if data
-            YAML.load(ActiveSupport::Base64.decode64(data)) 
+            Marshal.load(ActiveSupport::Base64.decode64(data)) 
           else
             {}
           end
-        end
-        
+        end        
       end
     end
   end
 rescue LoadError
-  # MemCache wasn't available so neither can the store be
+  # Redis wasn't available so neither can the store be
 end
